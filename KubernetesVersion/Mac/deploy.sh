062be97a77fb6a3c00a267e564763072546f54bd
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-09-10 16:49:40 +0800

    Update the config file

--- KubernetesVersion/Mac/deploy.sh
+++ KubernetesVersion/Mac/deploy.sh
@@ -12,14 +12,9 @@ sleep 5
 echo "1. 部署 NodeJS 应用..."
 kubectl apply -f nodeJSEnvironment.yaml
 
-sleep 5
-
 # 创建 NodeJS Service
 echo "2. 创建 NodeJS Service..."
 kubectl apply -f NodeJS-Service.yaml
-
-sleep 5
-
 # 创建 NodeJS HPA
 kubectl apply -f NodeJS-HorizontalPodAutoscaler.yaml
 
@@ -27,13 +22,9 @@ kubectl apply -f NodeJS-HorizontalPodAutoscaler.yaml
 echo "3. 创建 Nginx 配置..."
 kubectl apply -f Nginx-ConfigMap.yaml
 
-sleep 5
-
 # 部署 Nginx
 echo "4. 部署 Nginx..."
 kubectl apply -f Nginx/NginxEnvironment.yaml
-
-sleep 5
 
 # 创建 Nginx Service
 echo "5. 创建 Nginx Service..."
